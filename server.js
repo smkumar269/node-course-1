a0ef3b9c39fd1ab11ccfbb4496295ca9cbf143e1
Sankaranarayanan, Muthukumar <msankaranarayana@paypal.com>
2017-09-27 22:38:40 -0700

    add projects

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
 const port = process.env.PORT || 3000;
 var app = express();
 
@@ -40,6 +41,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
